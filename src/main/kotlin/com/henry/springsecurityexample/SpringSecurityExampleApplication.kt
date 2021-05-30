30f98c09a9addd11038808e09a827bfba9a122a5
henryjo <jobc0335@gmail.com>
2021-05-30 21:36:38 +0900

    init project

---
+++ src/main/kotlin/com/henry/springsecurityexample/SpringSecurityExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.henry.springsecurityexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringSecurityExampleApplication>(*args)
+}
